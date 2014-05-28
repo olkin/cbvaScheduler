758173420d9c01f81f1b1226206fbb5ee8ed53e5
olkin <pilip.olkin@gmail.com>
2014-05-28 16:51:26 -0600

    add sign-in and out functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
 
   root 'static_pages#home'
+  resources :sessions, only: [:new, :create, :destroy]
   match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
 
   resources :league_types
   resources :users
-
-  get 'welcome/index'
 
   resources :teams do
   	resources :matches
