4629996e3b52aeb16e26ca1a7ec6c1546de7413b
olkin <pilip.olkin@gmail.com>
2014-06-11 14:52:38 -0600

    add validations for cycles number

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,8 +1,15 @@
+require 'json'
+
 class League < ActiveRecord::Base
   has_many :teams, dependent: :destroy
   has_many :tier_settings, dependent: :destroy, inverse_of: :league
   validates :desc, presence: true, uniqueness: true
   validates :description, presence: true
+
+  DEFAULT_SCHEDULE_PATTERNS = {
+      2 => [[[[1,2,1]],[[1,2,1]],[[1,2,1]]]],
+      4 => [[[[1,4,1],[2,3,2]],[[1,3,1],[2,4,2]],[[1,2,1],[3,4,2]]]]
+  }
 
   def validate_settings
     add_error = Proc.new {
@@ -10,22 +17,51 @@ class League < ActiveRecord::Base
       return
     }
 
-    tier_settings = self.tier_settings
     add_error["No tiers settings"] if tier_settings.empty? or tier_settings.nil?
 
     tiers_supplied = []
+    cycles = {}
+    max_cycle = 0
     tier_settings.each{|setting|
+      add_error["Tier #{setting.tier} is invalid"] unless setting.valid?
       add_error["Tier #{setting.tier} is described multiple times"] if tiers_supplied.include?setting.tier
       tiers_supplied += [setting.tier]
+
+      cycles[setting.tier] =  JSON.parse(setting.schedule_pattern).size
+      max_cycle = cycles[setting.tier] if max_cycle < cycles[setting.tier]
     }
 
     if tiers_supplied.any?
+      tiers_supplied.uniq!
+      tiers_supplied.sort!
+
       missing_tiers = Array(1..tiers_supplied[-1]) - tiers_supplied
       add_error["No settings for #{'tier'.pluralize(missing_tiers.size)} #{missing_tiers.join(', ')}"] if missing_tiers.any?
+
+      for tier in 1..tiers_supplied[-1]
+        prev_tier_setting = tier_settings.all.find_by_tier(tier - 1)
+        cur_tier_setting = tier_settings.all.find_by_tier tier
+
+        teams_go_up = tier == 1 ? 0 : prev_tier_setting.teams_down
+
+        total_teams = cur_tier_setting.total_teams
+        add_error["Teams to move up can't exceed total number of teams in the tier(#{tier}) #{teams_go_up}/#{total_teams}"] \
+          if teams_go_up > total_teams
+
+        teams_go_down = tier == tiers_supplied[-1] ? 0 : cur_tier_setting.teams_down
+
+        add_error["Teams to move down can't exceed total number of teams in the tier(#{tier}) #{teams_go_down}/#{total_teams}"] \
+          if teams_go_down > total_teams
+
+        add_error["Teams to move up and down can't exceed total number of teams in the tier(#{tier}) #{teams_go_up}/#{teams_go_up}/#{total_teams}"] \
+          if teams_go_up + teams_go_down > total_teams
+
+        cycles.keep_if{|tier, cycles_nr| cycles_nr != max_cycle}
+
+        add_error["Number of cycles in #{'tier'.pluralize(cycles.count)} #{cycles.keys.join(', ')} differs from #{max_cycle}"] \
+          if cycles.any?
+      end
     end
-
-    # TODO: teams_down check
-
-    # TODO: cycles number should be same
+    true
   end
 end
--- spec/models/league_spec.rb
+++ spec/models/league_spec.rb
@@ -9,6 +9,7 @@ describe League do
   it { should respond_to(:description) }
   it { should respond_to(:start_date)}
   it { should respond_to(:teams) }
+  it { should respond_to(:tier_settings)}
 
   describe "when desc is not present" do
     before { league.desc = " " }
@@ -29,14 +30,49 @@ describe League do
     it { should_not be_valid }
   end
 
-  describe "when tier settings invalid" do
+  describe "when tier is missing" do
     before{
       league.save!
       FactoryGirl.create(:tier_setting, league: league, tier: 2)
+      league.validate_settings
     }
 
-    it {should_not be_valid}
+    it {league.validate_settings.should_not be_true}
 
   end
 
+  describe "when number of teams to move down is more than total number of teams" do
+    before{
+      league.save!
+      FactoryGirl.create(:tier_setting, league: league, tier: 1, total_teams: 4, teams_down: 3, schedule_pattern: League::DEFAULT_SCHEDULE_PATTERNS[4].to_s)
+      FactoryGirl.create(:tier_setting, league: league, tier: 2, total_teams: 2, teams_down: 0, schedule_pattern: League::DEFAULT_SCHEDULE_PATTERNS[2].to_s)
+      league.validate_settings
+    }
+
+    it {league.validate_settings.should_not be_true}
+  end
+
+  describe "when number of teams to move up+down is more than total number of teams" do
+    before{
+      league.save!
+      FactoryGirl.create(:tier_setting, league: league, tier: 1, total_teams: 4, teams_down: 2, schedule_pattern: League::DEFAULT_SCHEDULE_PATTERNS[4].to_s)
+      FactoryGirl.create(:tier_setting, league: league, tier: 2, total_teams: 2, teams_down: 2, schedule_pattern: League::DEFAULT_SCHEDULE_PATTERNS[2].to_s)
+      FactoryGirl.create(:tier_setting, league: league, tier: 3, total_teams: 2, teams_down: 2, schedule_pattern: League::DEFAULT_SCHEDULE_PATTERNS[2].to_s)
+      league.validate_settings
+    }
+
+    it {league.validate_settings.should_not be_true}
+  end
+
+  describe "when number of cycles differ in tiers" do
+    before{
+      league.save!
+      double_cycles = League::DEFAULT_SCHEDULE_PATTERNS[2]*2
+      FactoryGirl.create(:tier_setting, league: league, tier: 1)
+      FactoryGirl.create(:tier_setting, league: league, tier: 2, schedule_pattern: double_cycles.to_s)
+      league.validate_settings
+    }
+
+    it {league.validate_settings.should_not be_true}
+  end
 end
